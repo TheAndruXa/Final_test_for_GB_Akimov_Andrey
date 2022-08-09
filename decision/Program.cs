e15d8e7547bb0a09d49e0859ac0eee8aba1d7cd5
TheAndruXa <105748971+TheAndruXa@users.noreply.github.com>
2022-08-10 01:28:19 +0500

    Make a decision

---
+++ decision/Program.cs
@@ -0,0 +1,9 @@
+﻿string[] arr = new string[] {"hello", "2", "world", ":-"};
+
+for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3)
+        {
+            Console.WriteLine(arr[i]);   
+        }
+    }
